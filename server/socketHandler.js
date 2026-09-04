48895dc46c86da85fa734743475ebaa59533a40d
DnsMancini <103829552+DnsMancini@users.noreply.github.com>
2026-09-04 16:26:41 -0300

    fix: invalida timers de bot ao substituir por humano

--- server/socketHandler.js
+++ server/socketHandler.js
@@ -1,5 +1,36 @@
 const core = require('./socketHandler-core');
 
+function invalidateBotTimersOnHumanReplacement(room) {
+  if (!room?.game) return;
+  for (const timerName of ['botTurnTimer', 'botDecisionTimer', 'botResponseTimer']) {
+    if (room.game[timerName]) {
+      clearTimeout(room.game[timerName]);
+      room.game[timerName] = null;
+    }
+  }
+}
+
+const originalRoomsSet = core.rooms.set.bind(core.rooms);
+core.rooms.set = (roomCode, room) => {
+  if (room?.players && !room.players.__botReplacementGuard) {
+    const players = room.players;
+    const guardedPlayers = new Proxy(players, {
+      set(target, property, value) {
+        if (property !== 'length' && Number.isInteger(Number(property))) {
+          const index = Number(property);
+          if (target[index]?.isBot === true && value?.isBot === false) {
+            invalidateBotTimersOnHumanReplacement(room);
+          }
+        }
+        return Reflect.set(target, property, value);
+      }
+    });
+    Object.defineProperty(guardedPlayers, '__botReplacementGuard', { value: true });
+    room.players = guardedPlayers;
+  }
+  return originalRoomsSet(roomCode, room);
+};
+
 function handleSocket(io) {
   io.on('connection', (socket) => {
     socket.use((packet, next) => {
