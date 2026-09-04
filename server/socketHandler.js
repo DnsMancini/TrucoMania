5752c1ce596757350ff49cc2caa6940fd6906c22
DnsMancini <103829552+DnsMancini@users.noreply.github.com>
2026-09-04 16:37:09 -0300

    fix: impede reautenticacao de socket com outra identidade

--- server/socketHandler.js
+++ server/socketHandler.js
@@ -1,8 +1,14 @@
+const { Server } = require('socket.io');
 const core = require('./socketHandler-core');
 
 function handleSocket(io) {
   io.on('connection', (socket) => {
     socket.use((packet, next) => {
+      if (packet[0] === 'authenticate' && socket.user) {
+        socket.emit('authError', { message: 'Socket já autenticado. Reconecte para trocar de conta.' });
+        return next(new Error('Socket já autenticado'));
+      }
+
       if (packet[0] === 'playCard' && packet[1] && typeof packet[1] === 'object' && Number.isInteger(packet[1].blindIndex)) {
         packet[1] = { ...packet[1], suit: '4', rank: '4' };
       }
